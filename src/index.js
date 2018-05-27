549a8f644abbd52101e4a665bfe860ee1e3984fa
Nicole Attisano <nicoleattisano@Nicoles-MacBook-Pro.local>
2018-05-27 10:01:24 -0400

    changed structure of components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Link, Route, Switch} from 'react-router-dom'
-import HomePage from './containers/HomePage';
-import ArchivePage from './containers/ArchivePage';
-import SingleBookPage from './containers/SingleBookPage';
+import ArchivePage from './components/ArchivePage.js';
+import SingleBookPage from './components/SingleBookPage.js';
 
 ReactDOM.render((
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/archives" component={ArchivePage} exact/>
+        <Route path="/" component={ArchivePage} exact />
         <Route path="/book/:bookID" component={SingleBookPage} exact />
       </Switch>
     </BrowserRouter>
